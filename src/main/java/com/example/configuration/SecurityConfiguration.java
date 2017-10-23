64752f35564f451a980974440349dd7cbca56f0d
Monir <monir198@gmail.com>
2017-10-24 00:05:59 +0600

    testing push

--- src/main/java/com/example/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configuration/SecurityConfiguration.java
@@ -49,7 +49,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
+
 		http.
 			authorizeRequests()
 				.antMatchers("/").permitAll()
